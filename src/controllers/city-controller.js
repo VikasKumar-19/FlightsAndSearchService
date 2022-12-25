26e1d901edbb4e494f09e060f8eaff67bc635d50
Vikas Kumar <vikas.pratap98@gmail.com>
2022-12-25 23:20:43 +0530

    Added city controller

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,89 @@
+const { CityService } = require("../services");
+
+const cityService = new CityService();
+
+// POST -> /city -> req.body
+const create = async (req, res) => {
+  try {
+    const city = await cityService.createCity(req.body);
+    return res.status(201).json({
+      data: city,
+      success: true,
+      message: "Successfully created a city",
+      error: {},
+    });
+  } catch (err) {
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to create a city",
+      error: err,
+    });
+  }
+};
+
+// Patch -> /city/:id -> req.body
+const update = async (req, res) => {
+  try {
+    const city = await cityService.updateCity(req.params.id, req.body);
+    return res.status(200).json({
+      data: city,
+      success: true,
+      message: "Successfully updated the city",
+      error: {},
+    });
+  } catch (err) {
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to update the city",
+      error: err,
+    });
+  }
+};
+
+// DELETE -> /city/:id
+const destroy = async (req, res) => {
+  try {
+    const response = await cityService.deleteCity(req.params.id);
+    return res.status(200).json({
+      data: response,
+      success: true,
+      message: "Successfully deleted a city",
+      error: {},
+    });
+  } catch (err) {
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to update the city",
+      error: err,
+    });
+  }
+};
+
+const read = async (req, res) => {
+  try {
+    const city = await cityService.getCity(req.params.id);
+    return res.status(200).json({
+      data: city,
+      success: true,
+      message: "Successfully fetched the city",
+      error: {},
+    });
+  } catch (err) {
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to get the city",
+      error: err,
+    });
+  }
+};
+
+module.exports = {
+  create,
+  update,
+  destroy,
+  update,
+};
