c3db6b80de60755fca8fc71594461390153233a2
Vikas Kumar <vikas.pratap98@gmail.com>
2022-12-25 18:38:28 +0530

    created City model and CityRepository

---
+++ src/repository/city-repository.js
@@ -0,0 +1,28 @@
+const { City } = require("../models");
+
+class CityRepository {
+  async createCity({ name }) {
+    try {
+      const city = await City.create({
+        name,
+      });
+      return city;
+    } catch (error) {
+      throw { error };
+    }
+  }
+
+  async deleteCity(id) {
+    try {
+      await City.destroy({
+        where: {
+          id,
+        },
+      });
+    } catch (error) {
+      throw { error };
+    }
+  }
+}
+
+module.exports = CityRepository;
