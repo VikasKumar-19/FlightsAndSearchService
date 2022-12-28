284202f00fc620576f5de1e4d34f36c95f1f716f
Vikas Kumar <vk.pratap98@gmail.com>
2022-12-28 17:21:16 +0530

    created a helper function to compare time

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,4 +1,6 @@
 const { FlightRepository, AirplaneRepository } = require("../repository");
+const { compareTime } = require("../utils/helper");
+
 
 class FlightService {
   constructor() {
@@ -8,6 +10,10 @@ class FlightService {
 
   async createFlight(data) {
     try {
+      if(!compareTime(data.arrivalTime, data.departureTime)){
+        throw {error: "Arrival Time cannot be less than departure time"}
+      }
+
       const airplane = await this.airplaneRepository.getAirplane(
         data.airplaneId
       );
